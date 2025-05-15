76e7a8d1c2de5fe14dd64049c755ab670676588e
aidilbaihaqi <aidilmusirjun@gmail.com>
2025-05-15 22:47:28 +0700

    add starter file php

---
+++ v1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>INI CONTOH DOANG YEEE</title>
+</head>
+<body>
+  <p>ini contoh doang, ntar halaman index.php ini dijadiin halaman login. karena cara kerja web browser untuk landing page dia bakal nyari file index untuk dibuka pertama kali.</p>
+  <br>
+  <p>tapi bebas aja sih yang ngatur bang farrel, ada dua opsi</p>
+  <ul>
+    <li>jadiin halaman login menjadi index.php</li>
+    <li>buat redirect ke halaman login.php</li>
+  </ul>
+
+  <h4>SEMANGAT YEEEEEEEEEEEE</h4>
+</body>
+</html>
