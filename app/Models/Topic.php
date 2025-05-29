b2a4021d76ce36dbfe6ac03c838ea45c4ef5c97a
Aidil Baihaqi <aidilmusirjun@gmail.com>
2025-05-29 18:06:50 +0700

    many-many relationship: tag, topic

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Topic extends Model
@@ -26,5 +27,10 @@ class Topic extends Model
     public function likes(): MorphMany
     {
         return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'tag_topic')->withTimestamps();
     }
 }
---
+++ database/migrations/2025_05_29_110013_create_tag_topic_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tag_topic', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('topic_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+
+            $table->unique(['topic_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tag_topic');
+    }
+};
