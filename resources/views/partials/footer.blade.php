30b3d23c15408dbe39fe6d84b28187cc8cf43539
Aidil Baihaqi <aidilmusirjun@gmail.com>
2025-06-14 12:37:10 +0700

    setting navbar dan footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -9,19 +9,19 @@
             <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
                 <h5 class="text-uppercase mb-4">Tautan</h5>
                 <ul class="list-unstyled">
-                    <li class="mb-2"><a href="index.html" class="text-white">Home</a></li>
-                    <li class="mb-2"><a href="categories.html" class="text-white">Kategori</a></li>
-                    <li class="mb-2"><a href="popular.html" class="text-white">Populer</a></li>
-                    <li class="mb-2"><a href="about.html" class="text-white">Tentang</a></li>
+                    <li class="mb-2"><a href="{{ route("home.index") }}" class="text-white">Home</a></li>
+                    <li class="mb-2"><a href="" class="text-white">Kategori</a></li>
+                    <li class="mb-2"><a href="" class="text-white">Populer</a></li>
+                    <li class="mb-2"><a href="" class="text-white">Tentang</a></li>
                 </ul>
             </div>
             <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
                 <h5 class="text-uppercase mb-4">Akun</h5>
                 <ul class="list-unstyled">
-                    <li class="mb-2"><a href="login.html" class="text-white">Login</a></li>
-                    <li class="mb-2"><a href="register.html" class="text-white">Register</a></li>
-                    <li class="mb-2"><a href="profile.html" class="text-white">Profil</a></li>
-                    <li class="mb-2"><a href="settings.html" class="text-white">Pengaturan</a></li>
+                    <li class="mb-2"><a href="{{ route("login") }}" class="text-white">Login</a></li>
+                    <li class="mb-2"><a href="{{ route("register") }}" class="text-white">Register</a></li>
+                    <li class="mb-2"><a href="" class="text-white">Profil</a></li>
+                    <li class="mb-2"><a href="" class="text-white">Pengaturan</a></li>
                 </ul>
             </div>
             <div class="col-lg-4">
@@ -39,10 +39,10 @@
                 <p class="mb-0">&copy; 2025 SURAM. All rights reserved.</p>
             </div>
             <div class="col-md-6 text-center text-md-end">
-                <a href="#" class="text-white me-3"><i class="bi bi-facebook"></i></a>
-                <a href="#" class="text-white me-3"><i class="bi bi-twitter"></i></a>
-                <a href="#" class="text-white me-3"><i class="bi bi-instagram"></i></a>
-                <a href="#" class="text-white"><i class="bi bi-linkedin"></i></a>
+                <a href="https://facebook.com" class="text-white me-3"><i class="bi bi-facebook"></i></a>
+                <a href="https://x.com" class="text-white me-3"><i class="bi bi-twitter"></i></a>
+                <a href="https://instagram.com" class="text-white me-3"><i class="bi bi-instagram"></i></a>
+                <a href="https://linkedin.com" class="text-white"><i class="bi bi-linkedin"></i></a>
             </div>
         </div>
     </div>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,21 +7,21 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link active" href="index.html">Home</a>
+                    <a class="nav-link active" href="{{ route("home.index") }}">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="categories.html">Categories</a>
+                    <a class="nav-link" href="">Categories</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="popular.html">Popular</a>
+                    <a class="nav-link" href="">Popular</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="about.html">About</a>
+                    <a class="nav-link" href="">About</a>
                 </li>
             </ul>
             <div class="d-flex">
-                <a href="login.html" class="btn btn-outline-light me-2">Login</a>
-                <a href="register.html" class="btn btn-light">Register</a>
+                <a href="{{ route("login") }}" class="btn btn-outline-light me-2">Login</a>
+                <a href="{{ route("register") }}" class="btn btn-light">Register</a>
             </div>
         </div>
     </div>
